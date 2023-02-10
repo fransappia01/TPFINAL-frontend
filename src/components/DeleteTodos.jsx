5b23075a70542d67dd8fa59429ade8849f8133b9
fran <fransappia01@gmail.com>
2023-02-10 17:46:49 -0300

    delete alert

--- src/components/DeleteTodos.jsx
+++ src/components/DeleteTodos.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Button } from '@chakra-ui/react';
 import { deleteTodos } from '../api/todo';
-import {Swal} from 'sweetalert2'
+import Swal from 'sweetalert2'
 
 export default function DeleteTodo() {
   const queryClient = useQueryClient();
