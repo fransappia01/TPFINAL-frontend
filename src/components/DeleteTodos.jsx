de2653fc2e6296e75f0f4f0059966f600f6ac90f
fran <fransappia01@gmail.com>
2023-02-09 22:47:36 -0300

    alert

--- src/components/DeleteTodos.jsx
+++ src/components/DeleteTodos.jsx
@@ -3,19 +3,14 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Button } from '@chakra-ui/react';
 import { deleteTodos } from '../api/todo';
 import { Alert } from '@chakra-ui/react';
-import { useToast } from '@chakra-ui/react'
 
 export default function DeleteTodo() {
   const queryClient = useQueryClient();
-
-  const [open, setOpen] = React.useState(false);
-    const [deleteData, setDeleteData] = React.useState("");
 
   const deleteMutation = useMutation({
     mutationFn: deleteTodos,
     onSuccess: (data) => {
       queryClient.invalidateQueries({ queryKey: ['todos'] });
-      setOpen(true);
       setDeleteData(data.message);
     },
   })
@@ -31,14 +26,12 @@ export default function DeleteTodo() {
   return (
     <div className='add-menu'>
       <form onSubmit={handleSubmit}>
-        <Button type='submit' backgroundColor='red' px='8' onClick={handleDelete}>Delete All</Button>
-        {deleteMutation.isSuccess &&
-                    <useToast id="todolist-delete-snackbar" open={open} autoHideDuration={5000} onClose={handleClose} sx={{ width: '100%' }}>
+        <Button type='submit' backgroundColor='red' px='8' onClick={handleDelete}>Delete All
                         <Alert status='error'>
                          <AlertIcon />
                             Your ToDo's has been deleted
                         </Alert>
-                    </useToast>}
+        </Button>
       </form>
     </div>
   )
