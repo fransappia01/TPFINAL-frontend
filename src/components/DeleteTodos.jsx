87661cb1b740de08143adb2188ec6323e8244f6d
fran <fransappia01@gmail.com>
2023-02-10 01:05:04 -0300

    changes

--- src/components/DeleteTodos.jsx
+++ src/components/DeleteTodos.jsx
@@ -2,29 +2,14 @@ import React, { useState } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Button } from '@chakra-ui/react';
 import { deleteTodos } from '../api/todo';
-import { Snackbar } from '@mui/material';
-import { Alert } from '@mui/material';
-import {Divider } from '@mui/material'
 
 export default function DeleteTodo() {
   const queryClient = useQueryClient();
-
-  const [open, setOpen] = React.useState(false);
-  const [deleteData, setDeleteData] = React.useState("");
-
-  const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-        setOpen(false);
-    };
 
   const deleteMutation = useMutation({
     mutationFn: deleteTodos,
     onSuccess: (data) => {
       queryClient.invalidateQueries({ queryKey: ['todos'] });
-      setOpen(true);
-      setDeleteData(data.message);
     },
   })
 
@@ -41,12 +26,6 @@ export default function DeleteTodo() {
     <div className='add-menu'>
       <form onSubmit={handleSubmit}>
         <Button type='submit' backgroundColor='red' px='8' onClick={handleDelete}>Delete All</Button>
-        {deleteMutation.isSuccess &&
-                    <Snackbar id="todolist-delete-snackbar" open={open} autoHideDuration={5000} onClose={handleClose} sx={{ width: '100%' }}>
-                        <Alert severity="info" >
-                            Deleted
-                        </Alert>
-                    </Snackbar>}
       </form>
     </div>
   )
