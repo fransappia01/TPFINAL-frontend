a33c02840da1a3992400c7856b43b954c875c48a
fran <fransappia01@gmail.com>
2023-02-15 22:18:33 -0300

    codecept

---
+++ codecept/codecept.conf.js
@@ -0,0 +1,53 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  /*helpers: {
+    Playwright: {
+      url: 'https://deluxe-dust-production-isw3.up.railway.app/',
+      show: true,
+      browser: 'chromium'
+    }
+  }*/
+  helpers: {
+		REST: {
+			endpoint: "https://deluxe-dust-production-isw3.up.railway.app/",
+			onRequest: () => {
+			}
+		},
+    Playwright: {
+      url: 'https://deluxe-dust-production-isw3.up.railway.app/',
+      show: true,
+      browser: 'chromium'
+    }
+	},
+  mocha:  {
+    "reporterOptions": {
+      "codeceptjs-cli-reporter": {
+        "stdout": "-",
+        "options": {
+          "steps": true,
+        }
+      },
+      "mocha-junit-reporter": {
+        "stdout": "./output/console.log",
+        "options": {
+          "mochaFile": "./output/result.xml"
+        },
+        "attachments": true //add screenshot for a failed test
+		  }
+		}
+	},
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'codecept'
+}
---
+++ codecept/integration_test.js
@@ -0,0 +1,41 @@
+Feature('integration');
+
+const expect = require('chai').expect;
+const {I} = inject();
+
+require('dotenv').config()
+const backendURL = process.env.VITE_BACKEND_URL //problema con el meta env
+
+
+Scenario('Verify a successful call', async () => {
+	const res = await I.sendGetRequest(backendURL);
+	expect(res.status).to.eql(200);
+});
+
+
+Scenario('Title of the page', ({ I }) => {
+    I.amOnPage(backendURL);
+    I.see('To Do List'); 
+});
+
+Scenario('Deletes all todos',({ I }) => {
+    
+    I.amOnPage(backendURL);
+    I.click('Delete All');
+    I.see('All your tasks have been successfully deleted');
+});
+
+Scenario('Creates ToDos', ({I}) => {
+   
+    I.amOnPage(backendURL);
+    I.fillField('Title','Title Test');
+    I.fillField('Description','Description Test');
+    I.click('Add Task');
+    I.clearField('Title');
+    I.clearField('Description');
+    I.see('Title Test');
+    I.see('Description Test');
+    I.click('Delete All');
+    
+
+});
